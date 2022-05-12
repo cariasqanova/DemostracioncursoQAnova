527ac00fa10733e94e62bf57fbae4410982938e3
Cleinny Arias <cleinny.arias@externos.bci.cl>
2022-05-12 19:00:50 -0400

    Se agrega carpeta drive y src

---
+++ src/test/java/page/google.java
@@ -0,0 +1,11 @@
+package page;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class google {
+    @FindBy(className = "gLFyf gsfi")
+    WebElement inputBuscador;
+}
+@FindBy input[contains( data-ved="0ahUKEwjBto-Tg9v3AhU6ALkGHWGfA-MQ4dUDCAs")]
